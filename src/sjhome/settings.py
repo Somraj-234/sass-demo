9b228a422e5096955abd32cc870f595798dbe2e8
Somraj-234 <jadhavsomraj234@gmail.com>
2024-07-26 20:58:50 +0530

    updated build args

--- src/sjhome/settings.py
+++ src/sjhome/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
 DEBUG = config("DJANGO_DEBUG", cast=bool)
 
-print("DEBUG", DEBUG, type(DEBUG))
+# print("DEBUG", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     "sass-demo-production.up.railway.app",
@@ -97,7 +97,7 @@ DATABASES = {
 }
 
 CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
-DATABASE_URL = config("DATABASE_URL", cast=str)
+DATABASE_URL = config("DATABASE_URL", default=None)
 
 if DATABASE_URL is not None:
     import dj_database_url
@@ -108,8 +108,6 @@ if DATABASE_URL is not None:
             conn_health_checks=True
             )
     }
-
-
 
 
 # Password validation
