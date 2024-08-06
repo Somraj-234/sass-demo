64b5958ff0ca9d5cdba509cbfc1159e1022d17b1
Somraj-234 <jadhavsomraj234@gmail.com>
2024-08-06 23:48:46 +0530

    added stripe  billing_cycle_anchor to keep track of subscription if we need to extend it or something, also added status of subsciption.

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -149,6 +149,16 @@ class SubscriptionPrice(models.Model):
 
 
 class UserSubscription(models.Model):
+    class SubscriptionStatus(models.TextChoices):
+        ACTIVE = 'active', 'Active'
+        TRIALING = 'trialing','Trialing'
+        INCOMPLETE = 'incomplete','Incomplete'
+        INCOMPLETE_EXPIRED = 'incomplete_expired', 'Incomplete_Expired'
+        PAST_DUE = 'past_due', 'Past_Due'
+        CANCELED = 'canceled', 'Canceled'
+        UNPAID = 'unpaid', 'Unpaid'
+        PAUSED = 'paused', 'Paused'
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     subscription = models.ForeignKey(Subscription, on_delete=models.SET_NULL, null=True, blank=True)
     stripe_id = models.CharField(max_length=120, null=True, blank=True)
@@ -157,6 +167,21 @@ class UserSubscription(models.Model):
     original_period_start = models.DateField(auto_now=False, auto_now_add=False, blank=True, null=True)
     current_period_start = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
     current_period_end = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
+    status = models.CharField(max_length=20,
+    choices = SubscriptionStatus.choices,
+     null=True, blank=True)
+    
+
+
+    @property
+    def billing_cycle_anchor(self):
+        '''
+        https://docs.stripe.com/billing/subscriptions/billing-cycle#use-billing-cycle-anchor
+        Optional delay to start new subscription in stripe checkout 
+        '''
+        if not self.current_period_end:
+            return None
+        return int(self.current_period_end.timestamp())
 
     def save(self, *args, **kwargs):
          if (self.original_period_start is None and self.current_period_start is not None):
